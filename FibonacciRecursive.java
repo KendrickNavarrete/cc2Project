42e4c19b5ea430c175ceca751f77f541f1e91cae
KendrickNavarrete <44886023+KendrickNavarrete@users.noreply.github.com>
2018-11-23 15:58:38 +0800

    Create FibonacciRecursive.java

---
+++ FibonacciRecursive.java
@@ -0,0 +1,33 @@
+package javaapplication15;
+public class fibonacciRecursive {
+    public static void main(String[] args) {
+
+        int [] d = {3, 5, 7, 13, 2, 3, 9};
+            display (d);
+    }
+    
+    static void display (int []d){
+        for (int i = 0; i < d. length ; i++){
+                System.out.print("f("+ d[i] +")"+ "\t");
+                System.out.print(fibonacci (d[i]));
+                System.out.print("\n");
+            }
+        }
+    
+    static int fibonacci (int position){
+        if (position == 0){
+            return 0;
+        }
+        if (position == 1){
+            return 1;
+        }
+        else{
+            return fibonacci(position-1) + (position-2);
+        }
+    }
+}
+
+    © 2018 GitHub, Inc.
+    Terms
+    Privacy
+    Security
